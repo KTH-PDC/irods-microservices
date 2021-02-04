023be679383fae3f67622944001319a6fe1fd854
Ilari Korhonen <ilarik@kth.se>
2021-02-04 21:27:34 +0200

    base API plugin
    based on
    
    https://github.com/irods-contrib/irods_tools_lustre
    https://github.com/irods/irods_api_plugin_adapter
    https://github.com/irods/irods/blob/master/plugins/api/src/helloworld.cpp

---
+++ api_plugins.cmake
@@ -0,0 +1,62 @@
+set(
+  IRODS_API_PLUGIN_SOURCES_snic_serializer_api_server
+  ${CMAKE_SOURCE_DIR}/snic_serializer_api/libsnic_serializer_api_server.cpp
+  )
+
+set(
+  IRODS_API_PLUGIN_COMPILE_DEFINITIONS_snic_serializer_api_server
+  RODS_SERVER
+  ENABLE_RE
+  )
+
+set(
+  IRODS_API_PLUGIN_LINK_LIBRARIES_snic_serializer_api_server
+  irods_client
+  irods_server
+  irods_common
+  irods_plugin_dependencies
+  )
+
+set(
+  IRODS_API_PLUGINS
+  snic_serializer_api_server
+  )
+
+foreach(PLUGIN ${IRODS_API_PLUGINS})
+  add_library(
+    ${PLUGIN}
+    MODULE
+    ${IRODS_API_PLUGIN_SOURCES_${PLUGIN}}
+    )
+
+  target_include_directories(
+    ${PLUGIN}
+    PRIVATE
+    /usr/include
+    ${IRODS_INCLUDE_DIRS}
+    ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+    ${IRODS_EXTERNALS_FULLPATH_ARCHIVE}/include
+    )
+
+  target_link_libraries(
+    ${PLUGIN}
+    PRIVATE
+    ${IRODS_API_PLUGIN_LINK_LIBRARIES_${PLUGIN}}
+    ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so
+    ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+    ${IRODS_EXTERNALS_FULLPATH_ARCHIVE}/lib/libarchive.so
+    ${OPENSSL_CRYPTO_LIBRARY}
+    )
+
+  target_compile_definitions(${PLUGIN} PRIVATE ${IRODS_API_PLUGIN_COMPILE_DEFINITIONS_${PLUGIN}} ${IRODS_COMPILE_DEFINITIONS} BOOST_SYSTEM_NO_DEPRECATED)
+  target_compile_options(${PLUGIN} PRIVATE -Wno-write-strings)
+  set_property(TARGET ${PLUGIN} PROPERTY CXX_STANDARD ${IRODS_CXX_STANDARD})
+
+  install(
+    TARGETS
+    ${PLUGIN}
+    LIBRARY
+    DESTINATION usr/lib/irods/plugins/api
+    )
+
+endforeach()
